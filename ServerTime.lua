0fbd3304f5d29a5a3813533a0f52457e3f54daf6
redpawed <112795569+redpawed@users.noreply.github.com>
2022-11-03 17:10:48 +1100

    Update ServerTime.lua

--- ServerTime.lua
+++ ServerTime.lua
@@ -1,12 +1,14 @@
+local runservice = game:GetService"RunService"
+
 function formattime (seconds)
 	local pattern = "%02i:%02i:%02i"
 	local remainder = seconds % 60
-	
+
 	local minutes = (seconds - remainder) / 60
 	local hours = (minutes - minutes % 60) / 60
 	local seconds = seconds - (seconds - remainder)
 	minutes = minutes - hours * 60
-		
+
 	local format = string.format(pattern,
 		hours, minutes, seconds
 	)
@@ -15,7 +17,7 @@ function formattime (seconds)
 end
 
 local board = Instance.new"Part"
-board.Anchored = true
+board.Anchored, board.CanCollide, board.CanTouch = true, false, false
 board.Transparency = 1
 board.Size = Vector3.new(6, 4, 0)
 board.Material = Enum.Material.Marble
@@ -29,8 +31,9 @@ local back = Instance.new"TextBox"
 back.TextEditable, back.ClearTextOnFocus = false, false
 back.Size = UDim2.fromScale(1, 1)
 back.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
+back.BackgroundTransparency = 0.5
 back.TextColor3 = Color3.fromRGB(200, 200, 200)
-back.TextSize = 45
+back.TextSize = 70
 back.Text = ""
 back.Parent = surface
 
@@ -39,8 +42,14 @@ corner.CornerRadius = UDim.new(0, 40)
 corner.Parent = back
 
 task.spawn(function()
-	while true do task.wait(1/60)
-		board.CFrame = owner.Character.HumanoidRootPart.CFrame * CFrame.new(0, 6, 0)
-		back.Text = "Server Age:\n" .. formattime(time())
+	local x = 0
+	
+	while true do x = x + 1 task.wait(1/60)
+		local elapsed = formattime(time())
+		
+		board.CFrame = owner.Character.HumanoidRootPart.CFrame
+			* CFrame.new(0, 6 + math.sin(x/14) * 0.1, 0)
+			* CFrame.Angles(0, 0, math.rad(math.sin(x/28)*3))
+		back.Text = "Server Age:\n" .. elapsed
 	end
 end)
