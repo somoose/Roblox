9065c9f24adefacb50dce5978cc3a9aef9861117
somoose <112795569+somoose@users.noreply.github.com>
2024-08-18 08:14:20 +1000

    Create chair.lua

---
+++ LSB/furniture/serialised/chair.lua
@@ -0,0 +1,370 @@
+local Chair = Instance.new("Model")
+Chair.Name = "Chair"
+Chair.WorldPivot = CFrame.new(-24.51071548461914, 1.9211540222167969, -6.663142204284668)
+
+local Part = Instance.new("Part")
+Part.BottomSurface = Enum.SurfaceType.Smooth
+Part.TopSurface = Enum.SurfaceType.Smooth
+Part.Color = Color3.fromRGB(86, 66, 54)
+Part.Material = Enum.Material.Wood
+Part.Size = Vector3.new(0.3560771048069, 3.5373620986938477, 0.2439560443162918)
+Part.CFrame = CFrame.new(-25.521961212158203, 3.7813191413879395, -5.598299026489258)
+Part.Parent = Chair
+
+local BTWeld = Instance.new("Weld")
+BTWeld.Name = "BTWeld"
+BTWeld.C1 = CFrame.new(-1.0112439393997192, 1.8601652383804321, 1.0648434162139893)
+BTWeld.Parent = Part
+
+local BTWeld1 = Instance.new("Weld")
+BTWeld1.Name = "BTWeld"
+BTWeld1.C1 = CFrame.new(-1.011248230934143, 0.8868048787117004, 0.030494477599859238)
+BTWeld1.Parent = Part
+
+local BTWeld2 = Instance.new("Weld")
+BTWeld2.Name = "BTWeld"
+BTWeld2.C1 = CFrame.new(-2.0224878787994385, 0, 0)
+BTWeld2.Parent = Part
+
+local BTWeld3 = Instance.new("Weld")
+BTWeld3.Name = "BTWeld"
+BTWeld3.C1 = CFrame.new(-1.011248230934143, 2.5310442447662354, 1.9926155805587769)
+BTWeld3.Parent = Part
+
+local BTWeld4 = Instance.new("Weld")
+BTWeld4.Name = "BTWeld"
+BTWeld4.C1 = CFrame.new(-1.011248230934143, 2.5310442447662354, 0.030494477599859238)
+BTWeld4.Parent = Part
+
+local BTWeld5 = Instance.new("Weld")
+BTWeld5.Name = "BTWeld"
+BTWeld5.C1 = CFrame.new(-0.032959215342998505, 2.8664839267730713, 0.03049427643418312)
+BTWeld5.Parent = Part
+
+local BTWeld6 = Instance.new("Weld")
+BTWeld6.Name = "BTWeld"
+BTWeld6.C1 = CFrame.new(-1.011248230934143, -1.3516137599945068, 0.030494477599859238)
+BTWeld6.Parent = Part
+
+local BTWeld7 = Instance.new("Weld")
+BTWeld7.Name = "BTWeld"
+BTWeld7.C1 = CFrame.new(-1.956578016281128, 2.5310442447662354, 1.0368132591247559)
+BTWeld7.Parent = Part
+
+local BTWeld8 = Instance.new("Weld")
+BTWeld8.Name = "BTWeld"
+BTWeld8.C1 = CFrame.new(-1.011248230934143, 1.6796618700027466, 0.030494477599859238)
+BTWeld8.Parent = Part
+
+local BTWeld9 = Instance.new("Weld")
+BTWeld9.Name = "BTWeld"
+BTWeld9.C1 = CFrame.new(-0.03295493870973587, 2.8664839267730713, 2.0431313514709473)
+BTWeld9.Parent = Part
+
+local BTWeld10 = Instance.new("Weld")
+BTWeld10.Name = "BTWeld"
+BTWeld10.C1 = CFrame.new(-1.9895373582839966, 2.8664839267730713, 2.0431313514709473)
+BTWeld10.Parent = Part
+
+local BTWeld11 = Instance.new("Weld")
+BTWeld11.Name = "BTWeld"
+BTWeld11.C1 = CFrame.new(-1.9895329475402832, 2.8664839267730713, 0.03049427643418312)
+BTWeld11.Parent = Part
+
+local BTWeld12 = Instance.new("Weld")
+BTWeld12.Name = "BTWeld"
+BTWeld12.C1 = CFrame.new(-1.011248230934143, -0.7441881895065308, 0.030494477599859238)
+BTWeld12.Parent = Part
+
+local BTWeld13 = Instance.new("Weld")
+BTWeld13.Name = "BTWeld"
+BTWeld13.C1 = CFrame.new(-0.06592270731925964, 2.5310442447662354, 1.0368132591247559)
+BTWeld13.Parent = Part
+
+local BTWeld14 = Instance.new("Weld")
+BTWeld14.Name = "BTWeld"
+BTWeld14.C1 = CFrame.new(-1.0112457275390625, 1.8601651191711426, 1.0648434162139893)
+BTWeld14.Parent = Part
+
+local BTWeld15 = Instance.new("Weld")
+BTWeld15.Name = "BTWeld"
+BTWeld15.C1 = CFrame.new(-1.0112495422363281, 0.8868048191070557, 0.03049445152282715)
+BTWeld15.Parent = Part
+
+local BTWeld16 = Instance.new("Weld")
+BTWeld16.Name = "BTWeld"
+BTWeld16.C1 = CFrame.new(-2.0224876403808594, 0, 0)
+BTWeld16.Parent = Part
+
+local BTWeld17 = Instance.new("Weld")
+BTWeld17.Name = "BTWeld"
+BTWeld17.C1 = CFrame.new(-1.0112495422363281, 2.5310442447662354, 1.9926155805587769)
+BTWeld17.Parent = Part
+
+local BTWeld18 = Instance.new("Weld")
+BTWeld18.Name = "BTWeld"
+BTWeld18.C1 = CFrame.new(-1.0112495422363281, 2.5310442447662354, 0.03049445152282715)
+BTWeld18.Parent = Part
+
+local BTWeld19 = Instance.new("Weld")
+BTWeld19.Name = "BTWeld"
+BTWeld19.C1 = CFrame.new(-0.032958984375, 2.8664839267730713, 0.030494332313537598)
+BTWeld19.Parent = Part
+
+local BTWeld20 = Instance.new("Weld")
+BTWeld20.Name = "BTWeld"
+BTWeld20.C1 = CFrame.new(-1.0112495422363281, -1.3516135215759277, 0.03049445152282715)
+BTWeld20.Parent = Part
+
+local BTWeld21 = Instance.new("Weld")
+BTWeld21.Name = "BTWeld"
+BTWeld21.C1 = CFrame.new(-1.9565773010253906, 2.5310442447662354, 1.0368132591247559)
+BTWeld21.Parent = Part
+
+local BTWeld22 = Instance.new("Weld")
+BTWeld22.Name = "BTWeld"
+BTWeld22.C1 = CFrame.new(-1.0112495422363281, 1.679661750793457, 0.03049445152282715)
+BTWeld22.Parent = Part
+
+local BTWeld23 = Instance.new("Weld")
+BTWeld23.Name = "BTWeld"
+BTWeld23.C1 = CFrame.new(-0.032955169677734375, 2.8664839267730713, 2.0431313514709473)
+BTWeld23.Parent = Part
+
+local BTWeld24 = Instance.new("Weld")
+BTWeld24.Name = "BTWeld"
+BTWeld24.C1 = CFrame.new(-1.9895362854003906, 2.8664839267730713, 2.0431313514709473)
+BTWeld24.Parent = Part
+
+local BTWeld25 = Instance.new("Weld")
+BTWeld25.Name = "BTWeld"
+BTWeld25.C1 = CFrame.new(-1.989532470703125, 2.8664839267730713, 0.030494332313537598)
+BTWeld25.Parent = Part
+
+local BTWeld26 = Instance.new("Weld")
+BTWeld26.Name = "BTWeld"
+BTWeld26.C1 = CFrame.new(-1.0112495422363281, -0.7441883087158203, 0.03049445152282715)
+BTWeld26.Parent = Part
+
+local BTWeld27 = Instance.new("Weld")
+BTWeld27.Name = "BTWeld"
+BTWeld27.C1 = CFrame.new(-0.06592178344726562, 2.5310442447662354, 1.0368132591247559)
+BTWeld27.Parent = Part
+
+local Part1 = Instance.new("Part")
+Part1.BottomSurface = Enum.SurfaceType.Smooth
+Part1.TopSurface = Enum.SurfaceType.Smooth
+Part1.Color = Color3.fromRGB(86, 66, 54)
+Part1.Material = Enum.Material.Wood
+Part1.Size = Vector3.new(1.8906594514846802, 0.4780552387237549, 0.18296703696250916)
+Part1.CFrame = CFrame.new(-24.510711669921875, 5.132932662963867, -5.628793716430664)
+Part1.Parent = Chair
+
+local Part2 = Instance.new("Part")
+Part2.BottomSurface = Enum.SurfaceType.Smooth
+Part2.TopSurface = Enum.SurfaceType.Smooth
+Part2.Color = Color3.fromRGB(86, 66, 54)
+Part2.Material = Enum.Material.Wood
+Part2.Size = Vector3.new(1.8906594514846802, 0.361005574464798, 0.18296703696250916)
+Part2.CFrame = CFrame.new(-24.510711669921875, 4.52550745010376, -5.628793716430664)
+Part2.Parent = Chair
+
+local Part3 = Instance.new("Part")
+Part3.BottomSurface = Enum.SurfaceType.Smooth
+Part3.TopSurface = Enum.SurfaceType.Smooth
+Part3.Color = Color3.fromRGB(86, 66, 54)
+Part3.Material = Enum.Material.Wood
+Part3.Size = Vector3.new(1.8906594514846802, 0.4219946265220642, 0.18296703696250916)
+Part3.CFrame = CFrame.new(-24.510711669921875, 2.894514322280884, -5.628793716430664)
+Part3.Parent = Chair
+
+local Part4 = Instance.new("Part")
+Part4.BottomSurface = Enum.SurfaceType.Smooth
+Part4.TopSurface = Enum.SurfaceType.Smooth
+Part4.Color = Color3.fromRGB(86, 66, 54)
+Part4.Material = Enum.Material.Wood
+Part4.Size = Vector3.new(1.8906594514846802, 0.1780385971069336, 0.18296703696250916)
+Part4.CFrame = CFrame.new(-24.510711669921875, 2.1016573905944824, -5.628793716430664)
+Part4.Parent = Chair
+
+local Part5 = Instance.new("Part")
+Part5.BottomSurface = Enum.SurfaceType.Smooth
+Part5.TopSurface = Enum.SurfaceType.Smooth
+Part5.Color = Color3.fromRGB(86, 66, 54)
+Part5.Material = Enum.Material.Wood
+Part5.Size = Vector3.new(0.3000166416168213, 1.8296703100204468, 0.30494505167007446)
+Part5.CFrame = CFrame.new(-25.489002227783203, 0.9148352146148682, -5.628793239593506)
+Part5.Parent = Chair
+
+local Seat = Instance.new("Seat")
+Seat.Name = "Seat"
+Seat.BottomSurface = Enum.SurfaceType.Smooth
+Seat.TopSurface = Enum.SurfaceType.Smooth
+Seat.Color = Color3.fromRGB(86, 66, 54)
+Seat.Material = Enum.Material.Wood
+Seat.Size = Vector3.new(2.3785715103149414, 0.18296703696250916, 2.373642683029175)
+Seat.CFrame = CFrame.new(-24.51071548461914, 1.9211540222167969, -6.663142204284668)
+Seat.Parent = Chair
+
+local Part6 = Instance.new("Part")
+Part6.BottomSurface = Enum.SurfaceType.Smooth
+Part6.TopSurface = Enum.SurfaceType.Smooth
+Part6.Color = Color3.fromRGB(86, 66, 54)
+Part6.Material = Enum.Material.Wood
+Part6.Size = Vector3.new(2.0126373767852783, 0.3049450218677521, 0.18296703696250916)
+Part6.CFrame = CFrame.new(-24.510711669921875, 1.250274896621704, -5.628793716430664)
+Part6.Parent = Chair
+
+local Part7 = Instance.new("Part")
+Part7.BottomSurface = Enum.SurfaceType.Smooth
+Part7.TopSurface = Enum.SurfaceType.Smooth
+Part7.Color = Color3.fromRGB(86, 66, 54)
+Part7.Material = Enum.Material.Wood
+Part7.Size = Vector3.new(0.3000166416168213, 1.8296703100204468, 0.30494505167007446)
+Part7.CFrame = CFrame.new(-23.532428741455078, 0.9148352146148682, -5.628793239593506)
+Part7.Parent = Chair
+
+local Part8 = Instance.new("Part")
+Part8.BottomSurface = Enum.SurfaceType.Smooth
+Part8.TopSurface = Enum.SurfaceType.Smooth
+Part8.Color = Color3.fromRGB(86, 66, 54)
+Part8.Material = Enum.Material.Wood
+Part8.Size = Vector3.new(0.3560771048069, 3.5373620986938477, 0.2439560443162918)
+Part8.CFrame = CFrame.new(-23.499473571777344, 3.7813191413879395, -5.598299026489258)
+Part8.Parent = Chair
+
+local Part9 = Instance.new("Part")
+Part9.BottomSurface = Enum.SurfaceType.Smooth
+Part9.TopSurface = Enum.SurfaceType.Smooth
+Part9.Color = Color3.fromRGB(86, 66, 54)
+Part9.Material = Enum.Material.Wood
+Part9.Size = Vector3.new(0.12197796255350113, 0.3049450218677521, 1.707692265510559)
+Part9.CFrame = CFrame.new(-25.456039428710938, 1.250274896621704, -6.635112285614014)
+Part9.Parent = Chair
+
+local Part10 = Instance.new("Part")
+Part10.BottomSurface = Enum.SurfaceType.Smooth
+Part10.TopSurface = Enum.SurfaceType.Smooth
+Part10.Color = Color3.fromRGB(86, 66, 54)
+Part10.Material = Enum.Material.Wood
+Part10.Size = Vector3.new(0.12197796255350113, 0.3049450218677521, 1.707692265510559)
+Part10.CFrame = CFrame.new(-23.565383911132812, 1.250274896621704, -6.635112285614014)
+Part10.Parent = Chair
+
+local Part11 = Instance.new("Part")
+Part11.BottomSurface = Enum.SurfaceType.Smooth
+Part11.TopSurface = Enum.SurfaceType.Smooth
+Part11.Color = Color3.fromRGB(86, 66, 54)
+Part11.Material = Enum.Material.Wood
+Part11.Size = Vector3.new(0.3000166416168213, 1.8296703100204468, 0.30494505167007446)
+Part11.CFrame = CFrame.new(-23.532424926757812, 0.9148352146148682, -7.641430377960205)
+Part11.Parent = Chair
+
+local Part12 = Instance.new("Part")
+Part12.BottomSurface = Enum.SurfaceType.Smooth
+Part12.TopSurface = Enum.SurfaceType.Smooth
+Part12.Color = Color3.fromRGB(86, 66, 54)
+Part12.Material = Enum.Material.Wood
+Part12.Size = Vector3.new(0.3000166416168213, 1.8296703100204468, 0.30494505167007446)
+Part12.CFrame = CFrame.new(-25.48900604248047, 0.9148352146148682, -7.641430377960205)
+Part12.Parent = Chair
+
+local Part13 = Instance.new("Part")
+Part13.BottomSurface = Enum.SurfaceType.Smooth
+Part13.TopSurface = Enum.SurfaceType.Smooth
+Part13.Color = Color3.fromRGB(86, 66, 54)
+Part13.Material = Enum.Material.Wood
+Part13.Size = Vector3.new(2.0126373767852783, 0.3049450218677521, 0.18296703696250916)
+Part13.CFrame = CFrame.new(-24.510711669921875, 1.250274896621704, -7.590914726257324)
+Part13.Parent = Chair
+
+BTWeld.Part1 = Seat
+BTWeld.Part0 = Part
+
+BTWeld1.Part1 = Part3
+BTWeld1.Part0 = Part
+
+BTWeld2.Part1 = Part8
+BTWeld2.Part0 = Part
+
+BTWeld3.Part1 = Part13
+BTWeld3.Part0 = Part
+
+BTWeld4.Part1 = Part6
+BTWeld4.Part0 = Part
+
+BTWeld5.Part1 = Part5
+BTWeld5.Part0 = Part
+
+BTWeld6.Part1 = Part1
+BTWeld6.Part0 = Part
+
+BTWeld7.Part1 = Part10
+BTWeld7.Part0 = Part
+
+BTWeld8.Part1 = Part4
+BTWeld8.Part0 = Part
+
+BTWeld9.Part1 = Part12
+BTWeld9.Part0 = Part
+
+BTWeld10.Part1 = Part11
+BTWeld10.Part0 = Part
+
+BTWeld11.Part1 = Part7
+BTWeld11.Part0 = Part
+
+BTWeld12.Part1 = Part2
+BTWeld12.Part0 = Part
+
+BTWeld13.Part1 = Part9
+BTWeld13.Part0 = Part
+
+BTWeld14.Part1 = Seat
+BTWeld14.Part0 = Part
+
+BTWeld15.Part1 = Part3
+BTWeld15.Part0 = Part
+
+BTWeld16.Part1 = Part8
+BTWeld16.Part0 = Part
+
+BTWeld17.Part1 = Part13
+BTWeld17.Part0 = Part
+
+BTWeld18.Part1 = Part6
+BTWeld18.Part0 = Part
+
+BTWeld19.Part1 = Part5
+BTWeld19.Part0 = Part
+
+BTWeld20.Part1 = Part1
+BTWeld20.Part0 = Part
+
+BTWeld21.Part1 = Part10
+BTWeld21.Part0 = Part
+
+BTWeld22.Part1 = Part4
+BTWeld22.Part0 = Part
+
+BTWeld23.Part1 = Part12
+BTWeld23.Part0 = Part
+
+BTWeld24.Part1 = Part11
+BTWeld24.Part0 = Part
+
+BTWeld25.Part1 = Part7
+BTWeld25.Part0 = Part
+
+BTWeld26.Part1 = Part2
+BTWeld26.Part0 = Part
+
+BTWeld27.Part1 = Part9
+BTWeld27.Part0 = Part
+
+Chair.PrimaryPart = Seat
+Chair.Parent = workspace
+
+if owner.Character then Chair:PivotTo(owner.Character.HumanoidRootPart.CFrame + owner.Character.HumanoidRootPart.CFrame.LookVector * 5) end
+
+return Chair
