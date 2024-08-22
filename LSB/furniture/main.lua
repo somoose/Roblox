fc3a729869bcb0d1fca920553ac04cdf8b0c52b1
somoose <112795569+somoose@users.noreply.github.com>
2024-08-22 22:54:34 +1000

    Update main.lua

--- LSB/furniture/main.lua
+++ LSB/furniture/main.lua
@@ -1,6 +1,4 @@
-local HttpService = game:GetService("HttpService")
-
-local Furniture = { -- Make sure to return the final model.
+local Furniture = {
 	Bench = "https://raw.githubusercontent.com/somoose/Roblox/master/LSB/furniture/serialised/bench.lua",
 	Chair = "https://raw.githubusercontent.com/somoose/Roblox/master/LSB/furniture/serialised/chair.lua",
 	Stool = "https://raw.githubusercontent.com/somoose/Roblox/master/LSB/furniture/serialised/stool.lua",
@@ -8,34 +6,131 @@ local Furniture = { -- Make sure to return the final model.
 	Jenga = "https://raw.githubusercontent.com/somoose/Roblox/master/LSB/furniture/serialised/jenga.lua"
 }
 
-local args = {...}
-if args[1]:lower() == "get" then for name,_ in pairs(Furniture) do print(name) end return end
-local func
+local Keys = {}
 
-local amount = tonumber(args[2]) or 1
-local scale = tonumber(args[3]) or 1
+for Key, Value in pairs(Furniture) do
+	table.insert(Keys, Key)
+end
 
-for name, link in pairs(Furniture) do
-	if args[1]:lower() == name:lower():sub(1, #args[1]) then
-		func = loadstring(HttpService:GetAsync(link))
+local CollectionService = game:GetService("CollectionService")
+local furnTag = "__tag__"
+local HttpService = game:GetService("HttpService")
+
+local Prefix = "furn/"
+local LSBARGS = {...} -- r/arg1/arg2..
+
+local LoadedModels = {} -- Stores models which have already been loaded to prevent overuse of HttpService.
+
+function SpawnFurniture (Index, Amount, Scale) -- Returns a new piece of furniture matching Index, accepts truncated indices.
+	-- This function automatically positions the furniture in front of the owner and parents them to workspace.
+	Index = Index or Keys[math.random(#Keys)]
+	Amount = tonumber(Amount) or 1
+	Scale = tonumber(Scale) or 1
+	
+	for Name, Link in pairs(Furniture) do
+		if Name:lower():sub(1, #Index) == Index:lower() then
+			local CharacterSize = owner.Character:GetExtentsSize()
+			
+			local Models = {}
+			
+			for i = 1, Amount do				
+				if LoadedModels[Name] then -- Model has been loaded before.
+					local Clone = LoadedModels[Name]:Clone()
+					CollectionService:AddTag(Clone, furnTag)
+					table.insert(Models, Clone)
+				else -- Model has not been loaded before, and the link has to be processed through HttpService.
+					LoadedModels[Name] = loadstring(HttpService:GetAsync(Link))()
+					LoadedModels[Name].Parent = nil
+					
+					local Clone = LoadedModels[Name]:Clone()
+					CollectionService:AddTag(Clone, furnTag)
+					table.insert(Models, Clone)
+				end
+			end
+			
+			for i, Model in pairs(Models) do
+				Model.PrimaryPart = Model.PrimaryPart or Model:FindFirstChildOfClass("Part")
+				
+				Model:ScaleTo(Scale)
+				Model.Parent = workspace
+				
+				local ModelSize = Model:GetExtentsSize()
+				local Difference = Model:GetBoundingBox().Position - Model.PrimaryPart.Position
+				
+				local TargetCFrame = owner.Character:GetBoundingBox() * CFrame.new(
+					0,
+					ModelSize.Y/2 - CharacterSize.Y/2 + (i - 1) * ModelSize.Y,
+					-(CharacterSize.Z + ModelSize.Z)/2
+				) * CFrame.new(-Difference)
+				
+				Model:PivotTo(TargetCFrame)
+			end
+			
+			return Models
+		end
 	end
 end
 
-local mainmodel = func()
-mainmodel:ScaleTo(scale)
-mainmodel.Parent = workspace
-local modelsize = mainmodel:GetExtentsSize()
-mainmodel.Parent = nil
+function ClearAllModels ()
+	for _, Model in pairs(workspace:GetDescendants()) do
+		if Model:IsA("Model") then
+			if CollectionService:HasTag(Model, furnTag) then
+				Model:Destroy()
+			end
+		end
+	end
+end
 
-local characterpivot = owner.Character:GetPivot()
-local charactersize = owner.Character:GetExtentsSize()
-local feetheight = characterpivot * CFrame.new(0, -charactersize.Y/2, 0)
+local Commands = {
+	{
+		Code = "spawn",
+		Description = "Spawns the specified amount of models at the specified size.",
+		Arguments = "/index/amount/scale",
+		Function = SpawnFurniture
+	},
+	{
+		Code = "clear",
+		Description = "Clears all models with the " .. furnTag .. " tag.",
+		Arguments = "",
+		Function = ClearAllModels
+	},
+	{
+		Code = "cmds",
+		Arguments = "",
+		Description = "Prints all the commands."
+	}
+}
 
-local modelsize = mainmodel:GetExtentsSize()
-local modelpivot = feetheight * CFrame.new(0, 0, -charactersize.Z/2 + -modelsize.Z/2)
+Commands[3].Function = function () -- I set the function this way because I can't access the Commands table inside of the table.
+	for _, Command in pairs(Commands) do
+		warn("furn/" .. Command.Code .. Command.Arguments, Command.Description)
+	end
+end
 
-for i = 1, amount do task.wait()
-	local model = i ~= amount and mainmodel:Clone() or mainmodel
-	model:PivotTo(modelpivot * CFrame.new(0, (i - 1) * modelsize.Y, 0))
-	model.Parent = workspace
+if #LSBARGS > 0 then
+	for _, Command in pairs(Commands) do
+		if Command.Code:lower() == LSBARGS[1]:lower() then
+			table.remove(LSBARGS, 1)
+			Command.Function(table.unpack(LSBARGS))
+			
+			break
+		end
+	end
+else
+	owner.Chatted:Connect(function(Message)
+		if Message:sub(1, #Prefix):lower() == Prefix:lower() then
+			local Arguments = Message:sub(#Prefix + 1, #Message):split("/")
+			
+			for _, Command in pairs(Commands) do
+				if Arguments[1] then
+					if Command.Code:lower() == Arguments[1]:lower() then
+						table.remove(Arguments, 1)
+						Command.Function(table.unpack(Arguments))
+					end
+				end
+			end
+		end
+	end)
+
+	print("Say furn/cmds")
 end
