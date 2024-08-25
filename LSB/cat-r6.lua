a5239ceff5be8256cc43d86829e9de8b0db5a57b
somoose <112795569+somoose@users.noreply.github.com>
2024-08-25 10:32:58 +1000

    Update cat-r6.lua

--- LSB/cat-r6.lua
+++ LSB/cat-r6.lua
@@ -1,4 +1,15 @@
-local player = owner
+function GetPlayerFromTruncatedName (Name)
+	if not Name then return end
+	
+	for _, Player in pairs(game.Players:GetPlayers()) do
+		if Player.Name:lower():sub(1, #Name) == Name:lower() then return Player end
+		if Player.DisplayName:lower():sub(1, #Name) == Name:lower() then return Player end
+	end
+end
+
+local args = {...}
+
+local player = GetPlayerFromTruncatedName(args[1]) or owner
 local character = player.Character
 local humanoid = character.Humanoid
 
