642c038598707d2288aff0c582cd4b9fdc6397dc
somoose <112795569+somoose@users.noreply.github.com>
2024-08-27 16:19:54 +1000

    Create square-stool.lua

---
+++ LSB/furn/serialised/square-stool.lua
@@ -0,0 +1,126 @@
+local WoodenStool = Instance.new("Model")
+WoodenStool.Name = "WoodenStool"
+WoodenStool.WorldPivot = CFrame.new(-19.916667938232422, 3.291698932647705, -61.75, 0, 0, 1, 0, 0.9999990463256836, 0, -0.9999990463256836, 0, 0)
+
+local Part = Instance.new("Part")
+Part.BottomSurface = Enum.SurfaceType.Smooth
+Part.EnableFluidForces = false
+Part.TopSurface = Enum.SurfaceType.Smooth
+Part.Color = Color3.fromRGB(124, 92, 70)
+Part.Material = Enum.Material.Wood
+Part.Size = Vector3.new(0.41666659712791443, 2.0833330154418945, 0.41666659712791443)
+Part.CFrame = CFrame.new(-2.858966827392578, 2.0416979789733887, -33.06562042236328, 1, 0, 0, 0, 0.9999990463256836, 0, 0, 0, 0.9999990463256836)
+Part.Parent = WoodenStool
+
+local Part1 = Instance.new("Part")
+Part1.BottomSurface = Enum.SurfaceType.Smooth
+Part1.EnableFluidForces = false
+Part1.TopSurface = Enum.SurfaceType.Smooth
+Part1.Color = Color3.fromRGB(124, 92, 70)
+Part1.Material = Enum.Material.Wood
+Part1.Size = Vector3.new(2.499999523162842, 0.41666659712791443, 2.499999523162842)
+Part1.CFrame = CFrame.new(-1.817300796508789, 3.291696786880493, -32.023956298828125, 0, 0, 1, 0, 0.9999990463256836, 0, -0.9999990463256836, 0, 0)
+Part1.Parent = WoodenStool
+
+local Weld = Instance.new("Weld")
+Weld.C1 = CFrame.new(15.624998092651367, -1.0416665077209473, 8.958332061767578)
+Weld.C0 = CFrame.new(-7.916666030883789, -2.2916665077209473, 14.583332061767578, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+Weld.Parent = Part1
+
+local Weld1 = Instance.new("Weld")
+Weld1.C1 = CFrame.new(15.624998092651367, -1.4583332538604736, 7.916666030883789)
+Weld1.C0 = CFrame.new(-7.916666030883789, -2.2916665077209473, 14.583332061767578, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+Weld1.Parent = Part1
+
+local Weld2 = Instance.new("Weld")
+Weld2.C1 = CFrame.new(13.541665077209473, -1.0416665077209473, 8.958332061767578)
+Weld2.C0 = CFrame.new(-7.916666030883789, -2.2916665077209473, 14.583332061767578, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+Weld2.Parent = Part1
+
+local Weld3 = Instance.new("Weld")
+Weld3.C1 = CFrame.new(15.624998092651367, -1.0416665077209473, 6.874999046325684)
+Weld3.C0 = CFrame.new(-7.916666030883789, -2.2916665077209473, 14.583332061767578, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+Weld3.Parent = Part1
+
+local Weld4 = Instance.new("Weld")
+Weld4.C1 = CFrame.new(13.541665077209473, -1.4583332538604736, 7.916666030883789)
+Weld4.C0 = CFrame.new(-7.916666030883789, -2.2916665077209473, 14.583332061767578, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+Weld4.Parent = Part1
+
+local Weld5 = Instance.new("Weld")
+Weld5.C1 = CFrame.new(13.541665077209473, -1.0416665077209473, 6.874999046325684)
+Weld5.C0 = CFrame.new(-7.916666030883789, -2.2916665077209473, 14.583332061767578, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+Weld5.Parent = Part1
+
+local Part2 = Instance.new("Part")
+Part2.BottomSurface = Enum.SurfaceType.Smooth
+Part2.EnableFluidForces = false
+Part2.TopSurface = Enum.SurfaceType.Smooth
+Part2.Color = Color3.fromRGB(124, 92, 70)
+Part2.Material = Enum.Material.Wood
+Part2.Size = Vector3.new(0.41666659712791443, 0.41666659712791443, 1.6666663885116577)
+Part2.CFrame = CFrame.new(-2.858966827392578, 2.458364248275757, -32.023956298828125, 1, 0, 0, 0, 0.9999990463256836, 0, 0, 0, 0.9999990463256836)
+Part2.Parent = WoodenStool
+
+local Part3 = Instance.new("Part")
+Part3.BottomSurface = Enum.SurfaceType.Smooth
+Part3.EnableFluidForces = false
+Part3.TopSurface = Enum.SurfaceType.Smooth
+Part3.Color = Color3.fromRGB(124, 92, 70)
+Part3.Material = Enum.Material.Wood
+Part3.Size = Vector3.new(0.41666659712791443, 2.0833330154418945, 0.41666659712791443)
+Part3.CFrame = CFrame.new(-0.7756338119506836, 2.0416979789733887, -33.06562042236328, 1, 0, 0, 0, 0.9999990463256836, 0, 0, 0, 0.9999990463256836)
+Part3.Parent = WoodenStool
+
+local Part4 = Instance.new("Part")
+Part4.BottomSurface = Enum.SurfaceType.Smooth
+Part4.EnableFluidForces = false
+Part4.TopSurface = Enum.SurfaceType.Smooth
+Part4.Color = Color3.fromRGB(124, 92, 70)
+Part4.Material = Enum.Material.Wood
+Part4.Size = Vector3.new(0.41666659712791443, 2.0833330154418945, 0.41666659712791443)
+Part4.CFrame = CFrame.new(-2.858966827392578, 2.0416979789733887, -30.982290267944336, 1, 0, 0, 0, 0.9999990463256836, 0, 0, 0, 0.9999990463256836)
+Part4.Parent = WoodenStool
+
+local Part5 = Instance.new("Part")
+Part5.BottomSurface = Enum.SurfaceType.Smooth
+Part5.EnableFluidForces = false
+Part5.TopSurface = Enum.SurfaceType.Smooth
+Part5.Color = Color3.fromRGB(124, 92, 70)
+Part5.Material = Enum.Material.Wood
+Part5.Size = Vector3.new(0.41666659712791443, 0.41666659712791443, 1.6666663885116577)
+Part5.CFrame = CFrame.new(-0.7756338119506836, 2.458364248275757, -32.023956298828125, 1, 0, 0, 0, 0.9999990463256836, 0, 0, 0, 0.9999990463256836)
+Part5.Parent = WoodenStool
+
+local Part6 = Instance.new("Part")
+Part6.BottomSurface = Enum.SurfaceType.Smooth
+Part6.EnableFluidForces = false
+Part6.TopSurface = Enum.SurfaceType.Smooth
+Part6.Color = Color3.fromRGB(124, 92, 70)
+Part6.Material = Enum.Material.Wood
+Part6.Size = Vector3.new(0.41666659712791443, 2.0833330154418945, 0.41666659712791443)
+Part6.CFrame = CFrame.new(-0.7756338119506836, 2.0416979789733887, -30.982290267944336, 1, 0, 0, 0, 0.9999990463256836, 0, 0, 0, 0.9999990463256836)
+Part6.Parent = WoodenStool
+
+Weld.Part1 = Part
+Weld.Part0 = Part1
+
+Weld1.Part1 = Part2
+Weld1.Part0 = Part1
+
+Weld2.Part1 = Part3
+Weld2.Part0 = Part1
+
+Weld3.Part1 = Part4
+Weld3.Part0 = Part1
+
+Weld4.Part1 = Part5
+Weld4.Part0 = Part1
+
+Weld5.Part1 = Part6
+Weld5.Part0 = Part1
+
+WoodenStool.PrimaryPart = Part1
+WoodenStool.Parent = workspace.Furniture
+
+return WoodenStool
