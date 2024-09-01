07056985a3a97b42aa2d62130eae04bcda636cd3
somoose <112795569+somoose@users.noreply.github.com>
2024-09-01 21:20:35 +1000

    Update traffic-cone.lua

--- LSB/furn/serialised/traffic-cone.lua
+++ LSB/furn/serialised/traffic-cone.lua
@@ -5,5 +5,6 @@ Model.Name = "Traffic Cone"
 
 local TrafficCone = InsertService:CreateMeshPartAsync("http://www.roblox.com/asset/?id=1082802", Enum.CollisionFidelity.PreciseConvexDecomposition, Enum.RenderFidelity.Precise)
 TrafficCone.TextureID = "http://www.roblox.com/asset/?id=1082804"
+TrafficCone.Parent = Model
 
 return Model
