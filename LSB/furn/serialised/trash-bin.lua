314784f7291d109f937a599ecccbc7bfbec1a1c8
somoose <112795569+somoose@users.noreply.github.com>
2024-09-01 23:10:51 +1000

    Update trash-bin.lua

--- LSB/furn/serialised/trash-bin.lua
+++ LSB/furn/serialised/trash-bin.lua
@@ -4,6 +4,7 @@ local Model = Instance.new("Model")
 Model.Name = "Trash Bin"
 
 local TrashBin = InsertService:CreateMeshPartAsync("rbxassetid://8351096407", Enum.CollisionFidelity.PreciseConvexDecomposition, Enum.RenderFidelity.Precise)
+TrashBin.Material = Enum.Material.Metal
 TrashBin.Parent = Model
 
 return Model
