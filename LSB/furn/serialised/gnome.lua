e44ea5fc6c93b73ffce3ffa5123ea4224927b0fc
somoose <112795569+somoose@users.noreply.github.com>
2024-09-02 19:11:31 +1000

    Update gnome.lua

--- LSB/furn/serialised/gnome.lua
+++ LSB/furn/serialised/gnome.lua
@@ -5,6 +5,7 @@ Model.Name = "Gnome"
 
 local Gnome = InsertService:CreateMeshPartAsync("rbxassetid://14596253448", Enum.CollisionFidelity.Default, Enum.RenderFidelity.Automatic)
 Gnome.TextureID = "rbxassetid://14596254535"
+Gnome.Size = Gnome.Size * 0.25
 Gnome.Parent = Model
 
 return Model
