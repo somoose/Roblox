53a2dd89add3f90aad65579cd1b7352e9b7beef6
somoose <112795569+somoose@users.noreply.github.com>
2024-09-02 18:57:33 +1000

    Update trash-bin.lua

--- LSB/furn/serialised/trash-bin.lua
+++ LSB/furn/serialised/trash-bin.lua
@@ -3,7 +3,7 @@ local InsertService = game:GetService("InsertService")
 local Model = Instance.new("Model")
 Model.Name = "Trash Bin"
 
-local TrashBin = InsertService:CreateMeshPartAsync("rbxassetid://8351096407", Enum.CollisionFidelity.PreciseConvexDecomposition, Enum.RenderFidelity.Precise)
+local TrashBin = InsertService:CreateMeshPartAsync("rbxassetid://8351096407", Enum.CollisionFidelity.Default, Enum.RenderFidelity.Automatic)
 TrashBin.Material = Enum.Material.Metal
 TrashBin.Parent = Model
 
