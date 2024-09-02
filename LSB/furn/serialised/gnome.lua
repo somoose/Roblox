3022374d31dc7129720ef45305c438607918eb37
somoose <112795569+somoose@users.noreply.github.com>
2024-09-02 18:56:08 +1000

    Create gnome.lua

---
+++ LSB/furn/serialised/gnome.lua
@@ -0,0 +1,10 @@
+local InsertService = game:GetService("InsertService")
+
+local Model = Instance.new("Model")
+Model.Name = "Gnome"
+
+local Gnome = InsertService:CreateMeshPartAsync("rbxassetid://14596253448", Enum.CollisionFidelity.Default, Enum.RenderFidelity.Automatic)
+Gnome.TextureID = "rbxassetid://14596254535"
+Gnome.Parent = Model
+
+return Model
