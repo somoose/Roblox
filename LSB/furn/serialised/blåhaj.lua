f173984f49734cc8d6428faa39c0f99d5f229a34
somoose <112795569+somoose@users.noreply.github.com>
2024-09-03 16:05:38 +1000

    Create blåhaj.lua

---
+++ LSB/furn/serialised/blåhaj.lua
@@ -0,0 +1,10 @@
+local InsertService = game:GetService("InsertService")
+
+local Model = Instance.new("Model")
+Model.Name = "Blåhaj"
+
+local Blahaj = InsertService:CreateMeshPartAsync("rbxassetid://12291346966", Enum.CollisionFidelity.Default, Enum.RenderFidelity.Automatic)
+Blahaj.TextureID = "rbxassetid://12291347416"
+Blahaj.Parent = Model
+
+return Model
