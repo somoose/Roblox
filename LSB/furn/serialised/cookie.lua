4d08e7d295e6c45d9f8fa20a1fb004399702c6cc
somoose <112795569+somoose@users.noreply.github.com>
2024-09-05 18:03:15 +1000

    Update cookie.lua

--- LSB/furn/serialised/cookie.lua
+++ LSB/furn/serialised/cookie.lua
@@ -5,7 +5,7 @@ Model.Name = "Cookie"
 
 local Cookie = InsertService:CreateMeshPartAsync("rbxassetid://7240841424", Enum.CollisionFidelity.Default, Enum.RenderFidelity.Automatic)
 Cookie.TextureID = "rbxassetid://7240843295"
-Cookie.Size = Cookie.Size * 2
+Cookie.Size = Cookie.Size / 6
 Cookie.Parent = Model
 
 return Model
