6497953cbb6914a435a17915b162b49b7eff94d9
somoose <112795569+somoose@users.noreply.github.com>
2024-09-05 18:01:10 +1000

    Update cookie.lua

--- LSB/furn/serialised/cookie.lua
+++ LSB/furn/serialised/cookie.lua
@@ -4,13 +4,8 @@ local Model = Instance.new("Model")
 Model.Name = "Cookie"
 
 local Cookie = InsertService:CreateMeshPartAsync("rbxassetid://7240841424", Enum.CollisionFidelity.Default, Enum.RenderFidelity.Automatic)
+Cookie.TextureID = "rbxassetid://7240843295"
 Cookie.Size = Cookie.Size * 2
 Cookie.Parent = Model
-
-local SurfaceAppearance = Instance.new("SurfaceAppearance", Cookie)
-SurfaceAppearance.AlphaMode = Enum.AlphaMode.Transparency
-SurfaceAppearance.ColorMap = "rbxassetid://7240843295"
-SurfaceAppearance.NormalMap = "rbxassetid://7240845031"
-SurfaceAppearance.RoughnessMap = "rbxassetid://7240845588"
 
 return Model
