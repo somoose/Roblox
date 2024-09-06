d8ed7c70e80578fee60f9af3b71ef2e669b1f049
somoose <112795569+somoose@users.noreply.github.com>
2024-09-06 18:06:03 +1000

    Create conveyor.lua

---
+++ LSB/furn/serialised/conveyor.lua
@@ -0,0 +1,357 @@
+local Conveyor = Instance.new("Model")
+Conveyor.Name = "Conveyor"
+Conveyor.WorldPivot = CFrame.new(13.094856262207031, 1.3682664632797241, 0, 0, -0.25881901383399963, -0.9659258723258972, 0, 0.9659258723258972, -0.25881901383399963, 1, 0, 0)
+
+local Part = Instance.new("Part")
+Part.Anchored = true
+Part.BottomSurface = Enum.SurfaceType.Smooth
+Part.EnableFluidForces = false
+Part.TopSurface = Enum.SurfaceType.Smooth
+Part.Material = Enum.Material.Metal
+Part.Size = Vector3.new(4, 0.125, 12)
+Part.CFrame = CFrame.new(12.77635669708252, 3.4660696983337402, 0, 0, -0.258819043636322, -0.9659259915351868, 0, 0.9659259915351868, -0.258819043636322, 1, 0, 0)
+Part.Parent = Conveyor
+
+local Part1 = Instance.new("Part")
+Part1.Anchored = true
+Part1.BottomSurface = Enum.SurfaceType.Smooth
+Part1.EnableFluidForces = false
+Part1.TopSurface = Enum.SurfaceType.Smooth
+Part1.Material = Enum.Material.Metal
+Part1.Size = Vector3.new(12.125, 0.25, 0.75)
+Part1.CFrame = CFrame.new(12.837292671203613, 3.1953816413879395, -2.375, 0.9659259915351868, -0.258819043636322, 0, 0.258819043636322, 0.9659259915351868, 0, 0, 0, 1)
+Part1.Parent = Conveyor
+
+local Part2 = Instance.new("Part")
+Part2.Anchored = true
+Part2.BottomSurface = Enum.SurfaceType.Smooth
+Part2.EnableFluidForces = false
+Part2.TopSurface = Enum.SurfaceType.Smooth
+Part2.Material = Enum.Material.Metal
+Part2.Size = Vector3.new(0.375, 1.5, 4)
+Part2.CFrame = CFrame.new(7.138092994689941, 1.2813329696655273, 0, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part2.Parent = Conveyor
+
+local Part3 = Instance.new("Part")
+Part3.Anchored = true
+Part3.BottomSurface = Enum.SurfaceType.Smooth
+Part3.EnableFluidForces = false
+Part3.TopSurface = Enum.SurfaceType.Smooth
+Part3.Material = Enum.Material.Metal
+Part3.Size = Vector3.new(12.125, 0.5, 0.5)
+Part3.CFrame = CFrame.new(12.740337371826172, 3.557234287261963, 2.25, 0.9659259915351868, -0.258819043636322, 0, 0.258819043636322, 0.9659259915351868, 0, 0, 0, 1)
+Part3.Parent = Conveyor
+
+local Part4 = Instance.new("Part")
+Part4.Anchored = true
+Part4.BottomSurface = Enum.SurfaceType.Smooth
+Part4.EnableFluidForces = false
+Part4.TopSurface = Enum.SurfaceType.Smooth
+Part4.Material = Enum.Material.Metal
+Part4.Size = Vector3.new(12.125, 0.5, 0.5)
+Part4.CFrame = CFrame.new(12.740337371826172, 3.557234287261963, -2.25, 0.9659259915351868, -0.258819043636322, 0, 0.258819043636322, 0.9659259915351868, 0, 0, 0, 1)
+Part4.Parent = Conveyor
+
+local Part5 = Instance.new("Part")
+Part5.Anchored = true
+Part5.BottomSurface = Enum.SurfaceType.Smooth
+Part5.EnableFluidForces = false
+Part5.TopSurface = Enum.SurfaceType.Smooth
+Part5.Material = Enum.Material.Metal
+Part5.Size = Vector3.new(12.125, 0.25, 0.75)
+Part5.CFrame = CFrame.new(12.837289810180664, 3.1953816413879395, 2.375, 0.9659259915351868, -0.258819043636322, 0, 0.258819043636322, 0.9659259915351868, 0, 0, 0, 1)
+Part5.Parent = Conveyor
+
+local Part6 = Instance.new("Part")
+Part6.Anchored = true
+Part6.BottomSurface = Enum.SurfaceType.Smooth
+Part6.EnableFluidForces = false
+Part6.TopSurface = Enum.SurfaceType.Smooth
+Part6.Material = Enum.Material.Metal
+Part6.Size = Vector3.new(0.375, 1.600000023841858, 4)
+Part6.CFrame = CFrame.new(18.376569747924805, 4.3282341957092285, 0, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part6.Parent = Conveyor
+
+local Part7 = Instance.new("Part")
+Part7.Anchored = true
+Part7.BottomSurface = Enum.SurfaceType.Smooth
+Part7.EnableFluidForces = false
+Part7.TopSurface = Enum.SurfaceType.Smooth
+Part7.Material = Enum.Material.Metal
+Part7.Size = Vector3.new(0.5, 0.75, 3.5)
+Part7.CFrame = CFrame.new(18.314165115356445, 0.9067388772964478, -2.384185791015625e-07, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part7.Parent = Conveyor
+
+local Part8 = Instance.new("Part")
+Part8.Anchored = true
+Part8.BottomSurface = Enum.SurfaceType.Smooth
+Part8.EnableFluidForces = false
+Part8.TopSurface = Enum.SurfaceType.Smooth
+Part8.Material = Enum.Material.Metal
+Part8.Size = Vector3.new(10.75, 0.75, 0.5)
+Part8.CFrame = CFrame.new(12.694912910461426, 0.9067318439483643, -2.384185791015625e-07, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part8.Parent = Conveyor
+
+local Part9 = Instance.new("Part")
+Part9.Anchored = true
+Part9.BottomSurface = Enum.SurfaceType.Smooth
+Part9.EnableFluidForces = false
+Part9.TopSurface = Enum.SurfaceType.Smooth
+Part9.Material = Enum.Material.Metal
+Part9.Size = Vector3.new(0.375, 2.25, 0.625)
+Part9.CFrame = CFrame.new(18.376605987548828, 2.4052042961120605, -1.4375004768371582, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part9.Parent = Conveyor
+
+local Part10 = Instance.new("Part")
+Part10.Anchored = true
+Part10.BottomSurface = Enum.SurfaceType.Smooth
+Part10.EnableFluidForces = false
+Part10.TopSurface = Enum.SurfaceType.Smooth
+Part10.Material = Enum.Material.Metal
+Part10.Size = Vector3.new(0.375, 2.25, 0.625)
+Part10.CFrame = CFrame.new(18.376605987548828, 2.4052042961120605, 1.437499761581421, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part10.Parent = Conveyor
+
+local Part11 = Instance.new("Part")
+Part11.Anchored = true
+Part11.BottomSurface = Enum.SurfaceType.Smooth
+Part11.EnableFluidForces = false
+Part11.TopSurface = Enum.SurfaceType.Smooth
+Part11.Material = Enum.Material.Metal
+Part11.Size = Vector3.new(7, 0.375, 0.625)
+Part11.CFrame = CFrame.new(14.92621898651123, 2.292788505554199, -1.4375004768371582, 0.9659264087677002, 0.25881752371788025, 0, -0.25881755352020264, 0.9659263491630554, 0, 0, 0, 1)
+Part11.Parent = Conveyor
+
+local Part12 = Instance.new("Part")
+Part12.Anchored = true
+Part12.BottomSurface = Enum.SurfaceType.Smooth
+Part12.EnableFluidForces = false
+Part12.TopSurface = Enum.SurfaceType.Smooth
+Part12.Material = Enum.Material.Metal
+Part12.Size = Vector3.new(7, 0.375, 0.625)
+Part12.CFrame = CFrame.new(14.92621898651123, 2.292788505554199, 1.4374995231628418, 0.9659264087677002, 0.25881752371788025, 0, -0.25881755352020264, 0.9659263491630554, 0, 0, 0, 1)
+Part12.Parent = Conveyor
+
+local Part13 = Instance.new("Part")
+Part13.Anchored = true
+Part13.BottomSurface = Enum.SurfaceType.Smooth
+Part13.EnableFluidForces = false
+Part13.TopSurface = Enum.SurfaceType.Smooth
+Part13.Material = Enum.Material.Metal
+Part13.Size = Vector3.new(12.125, 0.25, 0.75)
+Part13.CFrame = CFrame.new(12.643380165100098, 3.919084072113037, -2.375, 0.9659259915351868, -0.258819043636322, 0, 0.258819043636322, 0.9659259915351868, 0, 0, 0, 1)
+Part13.Parent = Conveyor
+
+local Part14 = Instance.new("Part")
+Part14.Anchored = true
+Part14.BottomSurface = Enum.SurfaceType.Smooth
+Part14.EnableFluidForces = false
+Part14.TopSurface = Enum.SurfaceType.Smooth
+Part14.Material = Enum.Material.Metal
+Part14.Size = Vector3.new(12.125, 0.25, 0.75)
+Part14.CFrame = CFrame.new(12.643380165100098, 3.919084072113037, 2.375, 0.9659259915351868, -0.258819043636322, 0, 0.258819043636322, 0.9659259915351868, 0, 0, 0, 1)
+Part14.Parent = Conveyor
+
+local Part15 = Instance.new("Part")
+Part15.Anchored = true
+Part15.BottomSurface = Enum.SurfaceType.Smooth
+Part15.EnableFluidForces = false
+Part15.TopSurface = Enum.SurfaceType.Smooth
+Part15.Material = Enum.Material.Metal
+Part15.Size = Vector3.new(0.375, 0.5, 0.5)
+Part15.CFrame = CFrame.new(18.3765811920166, 0.2823706865310669, -2.25, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part15.Parent = Conveyor
+
+local Part16 = Instance.new("Part")
+Part16.Anchored = true
+Part16.BottomSurface = Enum.SurfaceType.Smooth
+Part16.EnableFluidForces = false
+Part16.TopSurface = Enum.SurfaceType.Smooth
+Part16.Material = Enum.Material.Metal
+Part16.Size = Vector3.new(0.375, 0.5, 0.5)
+Part16.CFrame = CFrame.new(18.3765811920166, 0.2823706865310669, 2.25, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part16.Parent = Conveyor
+
+local Part17 = Instance.new("Part")
+Part17.Anchored = true
+Part17.BottomSurface = Enum.SurfaceType.Smooth
+Part17.EnableFluidForces = false
+Part17.TopSurface = Enum.SurfaceType.Smooth
+Part17.Material = Enum.Material.Metal
+Part17.Size = Vector3.new(0.375, 0.5, 0.75)
+Part17.CFrame = CFrame.new(7.138093948364258, 0.2823599576950073, 2.375, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part17.Parent = Conveyor
+
+local Part18 = Instance.new("Part")
+Part18.Anchored = true
+Part18.BottomSurface = Enum.SurfaceType.Smooth
+Part18.EnableFluidForces = false
+Part18.TopSurface = Enum.SurfaceType.Smooth
+Part18.Material = Enum.Material.Metal
+Part18.Size = Vector3.new(0.375, 0.5, 0.75)
+Part18.CFrame = CFrame.new(7.138093948364258, 0.2823599576950073, -2.375, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Part18.Parent = Conveyor
+
+local Wedge = Instance.new("WedgePart")
+Wedge.Anchored = true
+Wedge.BottomSurface = Enum.SurfaceType.Smooth
+Wedge.EnableFluidForces = false
+Wedge.Material = Enum.Material.Metal
+Wedge.Size = Vector3.new(0.375, 0.25, 3)
+Wedge.CFrame = CFrame.new(18.37660026550293, 2.0305874347686768, -1.875000238418579, 1.0000001192092896, 0, 0, 0, 0, 1.0000001192092896, 0, -1, 0)
+Wedge.Parent = Conveyor
+
+local Wedge1 = Instance.new("WedgePart")
+Wedge1.Anchored = true
+Wedge1.BottomSurface = Enum.SurfaceType.Smooth
+Wedge1.EnableFluidForces = false
+Wedge1.Material = Enum.Material.Metal
+Wedge1.Size = Vector3.new(0.375, 0.25, 3)
+Wedge1.CFrame = CFrame.new(18.37660026550293, 2.0305874347686768, 1.874999761581421, -1.0000001192092896, 0, 0, 0, 0, 1.0000001192092896, 0, 1, 0)
+Wedge1.Parent = Conveyor
+
+local Wedge2 = Instance.new("WedgePart")
+Wedge2.Anchored = true
+Wedge2.BottomSurface = Enum.SurfaceType.Smooth
+Wedge2.EnableFluidForces = false
+Wedge2.Material = Enum.Material.Metal
+Wedge2.Size = Vector3.new(0.375, 0.375, 0.75)
+Wedge2.CFrame = CFrame.new(18.3765869140625, 0.719428300857544, -2.125, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Wedge2.Parent = Conveyor
+
+local Wedge3 = Instance.new("WedgePart")
+Wedge3.Anchored = true
+Wedge3.BottomSurface = Enum.SurfaceType.Smooth
+Wedge3.EnableFluidForces = false
+Wedge3.Material = Enum.Material.Metal
+Wedge3.Size = Vector3.new(0.375, 0.5, 0.75)
+Wedge3.CFrame = CFrame.new(18.3765811920166, 0.2823706865310669, -1.625, 1.0000001192092896, 0, 0, 0, -1.0000001192092896, 0, 0, 0, -1)
+Wedge3.Parent = Conveyor
+
+local Wedge4 = Instance.new("WedgePart")
+Wedge4.Anchored = true
+Wedge4.BottomSurface = Enum.SurfaceType.Smooth
+Wedge4.EnableFluidForces = false
+Wedge4.Material = Enum.Material.Metal
+Wedge4.Size = Vector3.new(0.375, 0.375, 0.75)
+Wedge4.CFrame = CFrame.new(18.3765869140625, 0.719428300857544, 2.125, -1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, -1)
+Wedge4.Parent = Conveyor
+
+local Wedge5 = Instance.new("WedgePart")
+Wedge5.Anchored = true
+Wedge5.BottomSurface = Enum.SurfaceType.Smooth
+Wedge5.EnableFluidForces = false
+Wedge5.Material = Enum.Material.Metal
+Wedge5.Size = Vector3.new(0.375, 0.5, 0.75)
+Wedge5.CFrame = CFrame.new(18.3765811920166, 0.2823706865310669, 1.625, -1.0000001192092896, 0, 0, 0, -1.0000001192092896, 0, 0, 0, 1)
+Wedge5.Parent = Conveyor
+
+local Wedge6 = Instance.new("WedgePart")
+Wedge6.Anchored = true
+Wedge6.BottomSurface = Enum.SurfaceType.Smooth
+Wedge6.EnableFluidForces = false
+Wedge6.Material = Enum.Material.Metal
+Wedge6.Size = Vector3.new(0.375, 0.5, 0.75)
+Wedge6.CFrame = CFrame.new(7.138095378875732, 0.28235864639282227, -1.625, 1.0000001192092896, 0, 0, 0, -1.0000001192092896, 0, 0, 0, -1)
+Wedge6.Parent = Conveyor
+
+local Wedge7 = Instance.new("WedgePart")
+Wedge7.Anchored = true
+Wedge7.BottomSurface = Enum.SurfaceType.Smooth
+Wedge7.EnableFluidForces = false
+Wedge7.Material = Enum.Material.Metal
+Wedge7.Size = Vector3.new(0.375, 0.375, 0.75)
+Wedge7.CFrame = CFrame.new(7.138086318969727, 0.7194100022315979, 2.375, -1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, -1)
+Wedge7.Parent = Conveyor
+
+local Wedge8 = Instance.new("WedgePart")
+Wedge8.Anchored = true
+Wedge8.BottomSurface = Enum.SurfaceType.Smooth
+Wedge8.EnableFluidForces = false
+Wedge8.Material = Enum.Material.Metal
+Wedge8.Size = Vector3.new(0.375, 0.375, 0.75)
+Wedge8.CFrame = CFrame.new(7.138086318969727, 0.7194100022315979, -2.375, 1.0000001192092896, 0, 0, 0, 1.0000001192092896, 0, 0, 0, 1)
+Wedge8.Parent = Conveyor
+
+local Wedge9 = Instance.new("WedgePart")
+Wedge9.Anchored = true
+Wedge9.BottomSurface = Enum.SurfaceType.Smooth
+Wedge9.EnableFluidForces = false
+Wedge9.Material = Enum.Material.Metal
+Wedge9.Size = Vector3.new(0.375, 0.5, 0.75)
+Wedge9.CFrame = CFrame.new(7.138095378875732, 0.28235864639282227, 1.625, -1.0000001192092896, 0, 0, 0, -1.0000001192092896, 0, 0, 0, 1)
+Wedge9.Parent = Conveyor
+
+local Conveyor1 = Instance.new("Part")
+Conveyor1.Name = "Conveyor"
+Conveyor1.Anchored = true
+Conveyor1.BottomSurface = Enum.SurfaceType.Smooth
+Conveyor1.EnableFluidForces = false
+Conveyor1.TopSurface = Enum.SurfaceType.Smooth
+Conveyor1.Color = Color3.fromRGB(27, 42, 53)
+Conveyor1.Material = Enum.Material.SmoothPlastic
+Conveyor1.Size = Vector3.new(4, 0.5, 12)
+Conveyor1.CFrame = CFrame.new(12.69556713104248, 3.767610549926758, 0, 0, -0.258819043636322, -0.9659259915351868, 0, 0.9659259915351868, -0.258819043636322, 1, 0, 0)
+Conveyor1.Parent = Conveyor
+
+local Attachment0 = Instance.new("Attachment")
+Attachment0.Name = "Attachment0"
+Attachment0.WorldSecondaryAxis = Vector3.new(-0, 0, -1)
+Attachment0.WorldOrientation = Vector3.new(15, -90, 90)
+Attachment0.CFrame = CFrame.new(0, 0.2579171657562256, 6, 0, -1, 0, 1, 0, -0, 0, 0, 1)
+Attachment0.WorldPosition = Vector3.new(6.83325719833374, 2.463825225830078, 0)
+Attachment0.Axis = Vector3.new(0, 1, 0)
+Attachment0.Rotation = Vector3.new(0, 0, 90)
+Attachment0.WorldCFrame = CFrame.new(6.83325719833374, 2.463825225830078, 0, -0.258819043636322, -0, -0.9659259915351868, 0.9659259915351868, 0, -0.258819043636322, 0, -1, 0)
+Attachment0.SecondaryAxis = Vector3.new(-1, 0, 0)
+Attachment0.Position = Vector3.new(0, 0.2579171657562256, 6)
+Attachment0.Orientation = Vector3.new(0, 0, 90)
+Attachment0.WorldAxis = Vector3.new(-0.258819043636322, 0.9659259915351868, 0)
+Attachment0.Parent = Conveyor1
+
+local Attachment1 = Instance.new("Attachment")
+Attachment1.Name = "Attachment1"
+Attachment1.WorldSecondaryAxis = Vector3.new(-0, 0, -1)
+Attachment1.WorldOrientation = Vector3.new(15, -90, 90)
+Attachment1.CFrame = CFrame.new(0, 0.2579171657562256, -6, 0, -1, 0, 1, 0, -0, 0, 0, 1)
+Attachment1.WorldPosition = Vector3.new(18.42436981201172, 5.569653511047363, 0)
+Attachment1.Axis = Vector3.new(0, 1, 0)
+Attachment1.Rotation = Vector3.new(0, 0, 90)
+Attachment1.WorldCFrame = CFrame.new(18.42436981201172, 5.569653511047363, 0, -0.258819043636322, -0, -0.9659259915351868, 0.9659259915351868, 0, -0.258819043636322, 0, -1, 0)
+Attachment1.SecondaryAxis = Vector3.new(-1, 0, 0)
+Attachment1.Position = Vector3.new(0, 0.2579171657562256, -6)
+Attachment1.Orientation = Vector3.new(0, 0, 90)
+Attachment1.WorldAxis = Vector3.new(-0.258819043636322, 0.9659259915351868, 0)
+Attachment1.Parent = Conveyor1
+
+local Beam = Instance.new("Beam")
+Beam.LightInfluence = 1
+Beam.Segments = 5
+Beam.TextureMode = Enum.TextureMode.Wrap
+Beam.TextureSpeed = 0
+Beam.Width1 = 4
+Beam.Width0 = 4
+Beam.TextureLength = 2
+Beam.Color = ColorSequence.new(Color3.fromRGB(0, 0, 0))
+Beam.Texture = "rbxassetid://9412762423"
+Beam.Parent = Conveyor1
+
+local SPEED = Instance.new("NumberValue")
+SPEED.Name = "SPEED"
+SPEED.Value = 10
+SPEED.Parent = Conveyor1
+
+Beam.Attachment0 = Attachment0
+Beam.Attachment1 = Attachment1
+
+function UpdateConveyor ()
+	Conveyor1.AssemblyLinearVelocity = Conveyor1.CFrame.LookVector * SPEED.Value * 2
+	Beam.TextureSpeed = SPEED.Value
+	Beam.Width0, Conveyor.Width1 = Conveyor1.Size.X, Conveyor1.Size.X
+end
+
+UpdateConveyor()
+
+SPEED:GetPropertyChangedSignal("Value"):Connect(UpdateConveyor)
+
+return Conveyor
