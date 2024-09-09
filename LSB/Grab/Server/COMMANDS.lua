0bd1f75033faca958c9a0add99b855fb7c0c2753
somoose <112795569+somoose@users.noreply.github.com>
2024-09-09 22:55:10 +1000

    Create COMMANDS.lua

---
+++ LSB/Grab/Server/COMMANDS.lua
@@ -0,0 +1,71 @@
+COMMAND_PREFIX = nil
+COMMAND_SEPARATOR = "/"
+
+COMMANDS = {
+	{
+		CODE = "bring",
+		ARGUMENTS = "player1" .. COMMAND_SEPARATOR .. "player2" .. COMMAND_SEPARATOR .. "...",
+		DESCRIPTION = "Brings the parsed players.",
+		FUNCTION = function (...)
+			local Names = {...}
+			
+			for _, Name in pairs(Names) do
+				local Target = FUNCTIONS.ReturnPlayer(Name)
+
+				if Target then
+					if Target.Character then
+						Target.Character:PivotTo(owner.Character:GetPivot())
+					end
+				end
+			end
+		end
+	},
+	{
+		CODE = "spawn",
+		FUNCTION = FURN.SpawnFurniture
+	},
+	{
+		CODE = "clear",
+		FUNCTION = FURN.ClearAllModels
+	},
+	{
+		CODE = "transparency",
+		FUNCTION = function (NewTransparency)
+			Transparency = tonumber(NewTransparency or 0.5) or 0.5
+		end
+	},
+	{
+		CODE = "sit",
+		FUNCTION = function (Boolean)
+			HumanoidSit = Boolean == "true" and true or false
+		end
+	},
+	{
+		CODE = "cancollide",
+		FUNCTION = function (Boolean)
+			CanCollide = Boolean == "true" and true or false
+		end
+	},
+	{
+		CODE = "get",
+		FUNCTION = function ()
+			print(ASSET_LIST)
+		end
+	}
+}
+
+FUNCTIONS.RUN_COMMAND = function (Text)
+	if COMMAND_PREFIX then
+		if Text:sub(1, 1):lower() ~= COMMAND_PREFIX:lower() then return end
+		Text = Text:sub(2, #Text)
+	end
+	
+	local Sections = Text:split(COMMAND_SEPARATOR)
+	
+	for _, Command in pairs(COMMANDS) do
+		if Sections[1]:lower() == Command.CODE:lower() then
+			table.remove(Sections, 1)
+			Command.FUNCTION(table.unpack(Sections))
+		end
+	end
+end
