b64cbd4d0a50c41a9b2eb79fea3d93cbf00c60c5
somoose <112795569+somoose@users.noreply.github.com>
2024-09-14 10:58:31 +1000

    Update BOBBLE.lua

--- LSB/Grab/Client/BOBBLE.lua
+++ LSB/Grab/Client/BOBBLE.lua
@@ -1,3 +1,12 @@
+FUNCTIONS.GetCurve = function (Frequency, Intensity) return math.sin(os.clock() * Frequency) * Intensity end
+
+FUNCTIONS.Lerp = function (a, b, t) return a + (b - a) * t end
+
+RotationFrequency, RotationIntensity = 6, 1
+BobbleFrequency, BobbleIntensity = 9, 0.085
+DefaultWalkSpeed = 16
+PreviousVelocity = nil
+
 RunService.PreRender:Connect(function(Delta)
 	local Velocity = math.round(Vector3.new(owner.Character.HumanoidRootPart.AssemblyLinearVelocity.X, 0, owner.Character.HumanoidRootPart.AssemblyLinearVelocity.Z).Magnitude)
 	
