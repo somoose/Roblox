c926666097f1ee3d8d307726d350b83c0d6f4ac0
somoose <112795569+somoose@users.noreply.github.com>
2024-09-19 19:17:19 +1000

    Create Character.lua

---
+++ LSB/BoxCharacter/Character.lua
@@ -0,0 +1,4 @@
+local HttpService = game:GetService("HttpService")
+loadstring(HttpService:GetAsync("https://raw.githubusercontent.com/somoose/Roblox/refs/heads/master/LSB/BoxCharacter/Module.lua"))()
+
+BoxCharacter.LoadCharacter(owner, true)
