5c2d0884a3bc118066ad50178d8b7a8a3fe4d406
somoose <112795569+somoose@users.noreply.github.com>
2024-10-08 21:54:32 +1100

    Update GameOfLife.lua

--- LSB/GameOfLife.lua
+++ LSB/GameOfLife.lua
@@ -11,11 +11,29 @@ local GameObj, GameModel = Life.new(X, Y, XS, YS)
 GameModel.Parent = script
 GameModel:PivotTo(CFrame.new(owner.Character.Head.Position + Vector3.new(0, owner.Character.Head.Size.Y/2 + 0.5, 0)))
 
+local GridPart = Instance.new("Part", GameModel)
+GridPart.Anchored = true
+GridPart.CanQuery = false
+GridPart.CanCollide = false
+GridPart.CanTouch = false
+GridPart.Size = Vector3.new(X, 0, Y)
+GridPart.CFrame = GameModel:GetPivot() * CFrame.new(0, GridPart.Size.Y/2 + 0.5, 0)
+GridPart.Transparency = 1
+
+local GridTexture = Instance.new("Texture", GridPart)
+GridTexture.Texture = "http://www.roblox.com/asset/?id=74407047721050"
+GridTexture.Face = Enum.NormalId.Top
+GridTexture.StudsPerTileU = XS
+GridTexture.StudsPerTileV = YS
+GridTexture.Color3 = Color3.fromRGB(50, 50, 50)
+
+local OutlineThickness = 1
+
 local Button = Instance.new("Part", GameModel)
 Button.Anchored = true
-Button.Size = Vector3.new(X, 1, 1)
-Button.CFrame = GameModel:GetPivot() * CFrame.new(0, 0, Y/2 + Button.Size.Z/2)
-Button.Color = Color3.fromRGB(255, 0, 0)
+Button.Color = GridTexture.Color3
+Button.Size = Vector3.new(X * XS + OutlineThickness*2, 0.99, Y * YS + OutlineThickness*2)
+Button.CFrame = GameModel:GetPivot() * CFrame.new(0, -(1 - Button.Size.Y), 0)
 Button.Material = Enum.Material.SmoothPlastic
 
 local ClickDetector = Instance.new("ClickDetector", Button)
