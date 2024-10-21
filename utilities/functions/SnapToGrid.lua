d69cf94a20fee0517b1358571729cd84e5fa71e5
somoose <112795569+somoose@users.noreply.github.com>
2024-10-21 19:27:31 +1100

    Update SnapToGrid.lua

--- utilities/functions/SnapToGrid.lua
+++ utilities/functions/SnapToGrid.lua
@@ -1,9 +1,7 @@
-function SnapToGrid (Vector, Scale)
-	local HalfScale = Scale / 2
-	
+local function SnapToGrid (Position, GridScale, GridOffset)
 	return Vector3.new(
-		math.round(Vector.X / Scale) * Scale + (Vector.X % Scale >= Scale/2 and -Scale/2 or Scale/2),
-		Vector.Y,
-		math.round(Vector.Z / Scale) * Scale + (Vector.Z % Scale >= Scale/2 and -Scale/2 or Scale/2)
+		math.round((Position.X - GridOffset.X) / GridScale.X) * GridScale.X + GridOffset.X,
+		math.round((Position.Y - GridOffset.Y) / GridScale.Y) * GridScale.Y + GridOffset.Y,
+		math.round((Position.Z - GridOffset.Z) / GridScale.Z) * GridScale.Z + GridOffset.Z
 	)
 end
